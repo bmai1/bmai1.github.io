373e558154308ea05651b4383fce1023856a4853
Brian Mai <0hrs3g@gmail.com>
2023-08-02 00:07:31 -0700

    onload opacity

---
+++ onload.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    const h1 = document.getElementById('honey');
+    const h2 = document.getElementById('honey2');
+    h1.style.opacity = "0.95";
+    h2.style.opacity = "0.95";
+};
--- preload.js
+++ preload.js
@@ -10,20 +10,11 @@ var imagesToPreload = [
 "images/cum.jpg",
 ];
 
-// Call the preloadImages function when the window has finished loading
 window.addEventListener("load", preloadImages);
 
 function preloadImages() {
     for (var i = 0; i < imagesToPreload.length; i++) {
         var image = new Image();
         image.src = imagesToPreload[i];
-    }
-    let url = window.location.href;
-    let fileName = url.substring(url.lastIndexOf('/') + 1);
-    if (fileName == "index.html") {
-        const h1 = document.getElementById('honey');
-        const h2 = document.getElementById('honey2');
-        h1.style.opacity = "0.95";
-        h2.style.opacity = "0.95";
     }
 }
