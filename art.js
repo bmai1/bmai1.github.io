f449809cb5397d9bef94244ba6675f96b24a5f60
Brian Mai <0hrs3g@gmail.com>
2023-08-06 23:05:28 -0700

    implement image viewer

---
+++ art.js
@@ -0,0 +1,14 @@
+const viewer = document.getElementById('viewer');
+let viewerActive = false;
+const openViewer = (image) => {
+    if (!viewerActive) {
+        viewer.style.display = "block";
+        viewer.innerHTML = `<img class="fadein" src="images/${image}" style="display: block; height: 100%; margin: auto;">`;
+        viewerActive = true;
+    }
+}
+const closeViewer = () => {
+    viewerActive = false;
+    viewer.innerHTML = '';
+    viewer.style.display = "none";
+}
