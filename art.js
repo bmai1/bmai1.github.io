a0582e42bbb66adc0870ae5bb2213d36a1e269bf
Brian Mai <0hrs3g@gmail.com>
2023-08-07 22:04:49 -0700

    scroll lock

--- art.js
+++ art.js
@@ -2,12 +2,14 @@ const viewer = document.getElementById('viewer');
 let viewerActive = false;
 const openViewer = (image) => {
     if (!viewerActive) {
+        document.body.style.overflow = 'hidden';
         viewer.style.display = "block";
         viewer.innerHTML = `<img class="fadein" src="images/${image}" style="display: block; height: 95%; margin: auto;">`;
         viewerActive = true;
     }
 }
 const closeViewer = () => {
+    document.body.style.overflow = 'auto';
     viewerActive = false;
     viewer.innerHTML = '';
     viewer.style.display = "none";
