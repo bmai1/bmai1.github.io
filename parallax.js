5ac0a5100a633203930be100e948606548524069
Brian Mai <0hrs3g@gmail.com>
2023-09-08 14:49:17 -0700

    add sunset transition

--- parallax.js
+++ parallax.js
@@ -10,6 +10,10 @@ window.addEventListener('scroll', () => {
    // Calculate the parallax effect for each layer
   const parallax = scrollPosition * 0.2;
 
+  document.querySelector('.sky').style.filter = `brightness(calc(100% - ${parallax * 0.7}%))`;
+  document.querySelector('.sun').style.transform = `translateY(${parallax * 7}px)`;
+  document.querySelector('.sun').style.filter = `saturate(calc(100% + ${parallax * 25}%))`;
+  
   document.querySelector('.layer1').style.transform = `translateY(${parallax * 3}px)`;
   document.querySelector('.layer2').style.transform = `translateY(${parallax * 1.3}px)`;
   document.querySelector('.layer3').style.transform = `translateY(${parallax * 2}px)`;
@@ -23,9 +27,11 @@ window.addEventListener('scroll', () => {
 
 
   if (scrollPosition >= window.innerHeight * 0.5 && scrollPosition < window.innerHeight * 2.5) {
+    
     sd1.classList.add('active');
     sd2.classList.remove('active');
     sd3.classList.remove('active');
+    document.querySelector('.sun').style.display = "none";
     welcomeDiv.style.opacity = 0;
   } 
   else if (scrollPosition >= window.innerHeight * 2.5 && scrollPosition < window.innerHeight * 5) {
@@ -39,6 +45,7 @@ window.addEventListener('scroll', () => {
     sd3.classList.add('active');
   }
   else {
+    document.querySelector('.sun').style.display = "block";
     sd1.classList.remove('active');
     sd2.classList.remove('active');
     sd3.classList.remove('active');
