fe329002219c977033200be895e9b8c8632db891
Brian Mai <0hrs3g@gmail.com>
2024-02-05 12:27:27 -0800

    trying to fix button display bug

--- parallax.js
+++ parallax.js
@@ -4,6 +4,7 @@ const sd3 = document.querySelector('.scroll-div3');
 const ed  = document.querySelector('.end-div');
 const welcomeDiv = document.querySelector('.welcome');
 
+
 const layers = document.querySelectorAll('.lax');
 
 window.addEventListener('scroll', () => {
@@ -32,7 +33,13 @@ window.addEventListener('scroll', () => {
     document.querySelector('.welcome').style.transform = `translateY(calc(10% + ${welcomeParallax}px))`;
   }
 
+  const r = document.querySelector('.rm'); // the one button the glitches out
+  console.log(r);
+
   if (scrollPosition >= window.innerHeight * 0.5 && scrollPosition < window.innerHeight * 1.5) {
+    
+    r.style.opacity = 0;
+
     sd1.classList.add('active');
     sd2.classList.remove('active');
     // sd3.classList.remove('active');
@@ -58,6 +65,7 @@ window.addEventListener('scroll', () => {
     ed.classList.add('active');
   }
   else {
+    r.style.opacity = 1;
     sd1.classList.remove('active');
     sd2.classList.remove('active');
     sd3.classList.remove('active');
